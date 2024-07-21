292bf489bee84857e11284b551276eae365bd391
DivyaDarshanTiwari <davidrocco400@outlook.com>
2024-07-22 03:07:58 +0530

    made the nav bar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:"",
+        loadComponent: ()=> import('../app/about/about.component').then((m)=> m.AboutComponent)
+    },
+    {
+        path:"Education",
+        loadComponent:()=> import('../app/education/education.component').then((m) => m.EducationComponent)
+    },
+    {
+        path:"Projects",
+        loadComponent:()=> import('../app/projects/projects.component').then((m) => m.ProjectsComponent)
+    }
+];
