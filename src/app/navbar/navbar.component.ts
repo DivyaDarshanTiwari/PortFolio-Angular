877c697a1adf0626f1752d14cf549c3cb93092e2
DivyaDarshanTiwari <davidrocco400@outlook.com>
2024-07-22 13:53:37 +0530

    added ripple

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,17 +1,19 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { CommonModule,NgFor } from '@angular/common';
+import { MatRipple } from '@angular/material/core';
 
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [RouterLink,CommonModule,NgFor],
+  imports: [RouterLink,CommonModule,NgFor,MatRipple],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
 })
 export class NavbarComponent {
 
-  protected pages=["Education", "Projects", "About"]
+  protected pages=["Education", "Projects", "About"];
 
+  protected ripple_color:string = "rgba(0, 0, 0, 1)";
 }
