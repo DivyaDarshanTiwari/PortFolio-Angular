c82e5db0a287d78153abba0aa9b77ecf8e3e4ac3
DivyaDarshanTiwari <davidrocco400@outlook.com>
2024-07-22 13:06:12 +0530

    data

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,12 +1,22 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import { CommonModule, NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-about',
   standalone: true,
-  imports: [],
+  imports: [MatCardModule,CommonModule,NgFor],
   templateUrl: './about.component.html',
-  styleUrl: './about.component.scss'
+  styleUrl: './about.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AboutComponent {
+  protected characteristics: string[] = [
+    'Leadership',
+    'Management',
+    'Organization',
+    'TeamWork',
+  ];
 
+  protected image:string = "Profile_pic.jpg"
 }
