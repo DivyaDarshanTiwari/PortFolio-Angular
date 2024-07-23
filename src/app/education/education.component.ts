2297305ae760fc4eb4a5af71a576410040b70775
DivyaDarshanTiwari <davidrocco400@outlook.com>
2024-07-24 01:22:03 +0530

    skill and education added

--- src/app/education/education.component.ts
+++ src/app/education/education.component.ts
@@ -1,12 +1,70 @@
 import { Component } from '@angular/core';
+import { CommonModule, NgFor } from '@angular/common';
+import { LengthControlPipe } from '../Shared_Pipe/Length_control_Pipe/length-control.pipe';
+import {MatTooltipModule} from '@angular/material/tooltip';
+
+type college = {
+  year: number;
+  name: string;
+  city: string;
+  class: string;
+  percentage: string;
+};
+
+type skills = {
+  icon: string;
+  icon_alt: string;
+  heading: string;
+  details: string;
+};
 
 @Component({
   selector: 'app-education',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, NgFor,LengthControlPipe,MatTooltipModule],
   templateUrl: './education.component.html',
-  styleUrl: './education.component.scss'
+  styleUrl: './education.component.scss',
 })
 export class EducationComponent {
+  protected college: college[] = [
+    {
+      year: 2017,
+      name: 'City Montessorie School',
+      city: 'Lucknow',
+      class: 'Class 10',
+      percentage: '93.6%',
+    },
+    {
+      year: 2019,
+      name: 'Don Bosco Academy',
+      city: 'Patna',
+      class: 'Class 12',
+      percentage: '73%',
+    },
+    {
+      year: 2022,
+      name: 'UPES',
+      city: 'Deharadun',
+      class: 'CPGA',
+      percentage: '8.66',
+    },
+  ];
 
+  protected skills: skills[] = [
+    {
+      icon: 'favicon.ico',
+      icon_alt: 'Angular',
+      heading: 'Project work in Angular',
+      details: `Proficient in creating responsive and accessible web applications
+          using HTML5, CSS3 (including Flexbox, Grid, and Sass), and Angular
+          framework, with experience in component-based architecture,
+          TypeScript, RxJS, and state management.`,
+    },
+    {
+      icon: 'Github.png',
+      icon_alt: 'Github',
+      heading: 'Colaboration and Communication',
+      details: `Skilled in fostering effective teamwork and clear communication, which are crucial for successful project outcomes. Proficient in utilizing version control systems like Git and collaboration platforms such as GitHub to streamline workflow and enhance team productivity`,
+    },
+  ];
 }
