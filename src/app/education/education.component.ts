ea156d77997cbc3182332e83a7492d9015ffc1f3
DivyaDarshanTiwari <davidrocco400@outlook.com>
2024-07-24 06:54:29 +0530

    document section added

--- src/app/education/education.component.ts
+++ src/app/education/education.component.ts
@@ -3,7 +3,8 @@ import { CommonModule, NgFor } from '@angular/common';
 import { LengthControlPipe } from '../Shared_Pipe/Length_control_Pipe/length-control.pipe';
 import { MatTooltipModule } from '@angular/material/tooltip';
 import { RailerComponent } from '../railer/railer.component';
-
+import { MatCardModule } from '@angular/material/card';
+import { MatIcon } from '@angular/material/icon';
 type college = {
   year: number;
   name: string;
@@ -18,11 +19,23 @@ type skills = {
   heading: string;
   details: string;
 };
+type document ={
+  document_name: string,
+  documnet_link: string,
+}
 
 @Component({
   selector: 'app-education',
   standalone: true,
-  imports: [CommonModule, NgFor, LengthControlPipe, MatTooltipModule,RailerComponent],
+  imports: [
+    CommonModule,
+    NgFor,
+    LengthControlPipe,
+    MatTooltipModule,
+    RailerComponent,
+    MatCardModule,
+    MatIcon
+  ],
   templateUrl: './education.component.html',
   styleUrl: './education.component.scss',
 })
@@ -66,6 +79,29 @@ export class EducationComponent {
       icon_alt: 'Github',
       heading: 'Colaboration and Communication',
       details: `Skilled in fostering effective teamwork and clear communication, which are crucial for successful project outcomes. Proficient in utilizing version control systems like Git and collaboration platforms such as GitHub to streamline workflow and enhance team productivity`,
+    },
+  ];
+
+  protected documents:document[] = [
+    {
+      document_name: 'CV',
+      documnet_link: 'CV.pdf',
+    },
+    {
+      document_name: 'CSA Hackthon 3.0',
+      documnet_link: 'CSA Hackathon 3.0.pdf',
+    },
+    {
+      document_name: 'Srijan Social Intership',
+      documnet_link: 'Shrijan Social Internship.jpg',
+    },
+    {
+      document_name: 'Yeh Ek Soch LOR',
+      documnet_link: 'Yes LOR.jpg',
+    },
+    {
+      document_name: 'Innovation for Change',
+      documnet_link: 'IFC Cerificate.jpg',
     },
   ];
 }
