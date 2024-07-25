1195cf99dba1500ef4bbd87611bbcdde82a5341a
DivyaDarshanTiwari <davidrocco400@outlook.com>
2024-07-25 17:49:16 +0530

    prject section

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -1,12 +1,44 @@
-import { Component } from '@angular/core';
+import { CommonModule,NgFor } from '@angular/common';
+import {ChangeDetectionStrategy, Component } from '@angular/core';
+import {MatCardModule} from '@angular/material/card';
+
+type project={
+  project_name:string,
+  features:string[],
+  img:string
+}
 
 @Component({
   selector: 'app-projects',
   standalone: true,
-  imports: [],
+  imports: [MatCardModule,CommonModule,NgFor],
   templateUrl: './projects.component.html',
-  styleUrl: './projects.component.scss'
+  styleUrl: './projects.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ProjectsComponent {
+
+  protected projects:project[]=[
+    {
+      project_name:"Dummy_Hotstar",
+      features:["Roubust", "Secure"],
+      img:"Dummy_Hotstar.png"
+    },
+    {
+      project_name:"BitCoin",
+      features:["SimpleUI", "MultiCoins","MultiCurrency"],
+      img:"BitCoin.png"
+    },
+    {
+      project_name:"TodoList",
+      features:["SimpleUI", "Priority"],
+      img:"TodoList.png"
+    },
+    {
+      project_name:"Invoice Genetation Landing page",
+      features:["SimpleUI", "Detailed"],
+      img:"InvoiceG.png"
+    },
+  ]
 
 }
