565f55a7efdeb388032c49cece1022222d5583a1
DivyaDarshanTiwari <davidrocco400@outlook.com>
2024-07-25 19:11:13 +0530

    project create complete

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -1,44 +1,53 @@
-import { CommonModule,NgFor } from '@angular/common';
-import {ChangeDetectionStrategy, Component } from '@angular/core';
-import {MatCardModule} from '@angular/material/card';
+import { CommonModule, NgFor } from '@angular/common';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
 
-type project={
-  project_name:string,
-  features:string[],
-  img:string
-}
+type project = {
+  project_name: string;
+  features: string[];
+  img: string;
+  link: string;
+};
 
 @Component({
   selector: 'app-projects',
   standalone: true,
-  imports: [MatCardModule,CommonModule,NgFor],
+  imports: [MatCardModule, CommonModule, NgFor],
   templateUrl: './projects.component.html',
   styleUrl: './projects.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ProjectsComponent {
-
-  protected projects:project[]=[
+  protected projects: project[] = [
     {
-      project_name:"Dummy_Hotstar",
-      features:["Roubust", "Secure"],
-      img:"Dummy_Hotstar.png"
+      project_name: 'Dummy_Hotstar',
+      features: ['Roubust', 'Secure'],
+      img: 'Dummy_Hotstar.png',
+      link: 'https://divyadarshantiwari.github.io/Dummy_Hotstar/',
     },
     {
-      project_name:"BitCoin",
-      features:["SimpleUI", "MultiCoins","MultiCurrency"],
-      img:"BitCoin.png"
+      project_name: 'BitCoin',
+      features: ['SimpleUI', 'MultiCoins', 'MultiCurrency'],
+      img: 'BitCoin.png',
+      link: 'https://divyadarshantiwari.github.io/Bitcoin/',
     },
     {
-      project_name:"TodoList",
-      features:["SimpleUI", "Priority"],
-      img:"TodoList.png"
+      project_name: 'TodoList',
+      features: ['SimpleUI', 'Priority'],
+      img: 'TodoList.png',
+      link: '',
     },
     {
-      project_name:"Invoice Genetation Landing page",
-      features:["SimpleUI", "Detailed"],
-      img:"InvoiceG.png"
+      project_name: 'Invoice Genetation Landing page',
+      features: ['SimpleUI', 'Detailed'],
+      img: 'InvoiceG.png',
+      link: 'https://divyadarshantiwari.github.io/Add-page-InvoiceGenerator/',
     },
-  ]
-
+    {
+      project_name: 'Product Price Landing Page',
+      features: ['HTML', 'CSS'],
+      img: 'ProductPrice.png',
+      link: 'https://divyadarshantiwari.github.io/Product-Price-Page/',
+    },
+  ];
 }
