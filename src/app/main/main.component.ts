64c577745b7b3ce8ad38a47d47ebed8e21731e22
DivyaDarshanTiwari <davidrocco400@outlook.com>
2024-07-31 09:45:58 +0530

    to the top icon added

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -3,11 +3,12 @@ import { AboutComponent } from '../about/about.component';
 import { EducationComponent } from '../education/education.component';
 import { ProjectsComponent } from '../projects/projects.component';
 import { LazyLoadImageDirective } from '../Directives/lazy-load-image.directive';
+import {MatIconModule} from '@angular/material/icon';
 
 @Component({
   selector: 'app-main',
   standalone: true,
-  imports: [AboutComponent,EducationComponent,ProjectsComponent,LazyLoadImageDirective],
+  imports: [AboutComponent,EducationComponent,ProjectsComponent,LazyLoadImageDirective,MatIconModule],
   templateUrl: './main.component.html',
   styleUrl: './main.component.scss'
 })
